b031dea3233572d0b16da233cfc6fedb33f49be4
Thiago laian <thiago.laian@gmail.com>
2020-08-22 06:08:00 -0300

    adicionado a função de copiar o texto

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     document.getElementById("btn-uppercase").addEventListener("click", upperCase);
     document.getElementById("btn-lowercase").addEventListener("click", lowerCase);
+    document.getElementById("btn-clipboard").addEventListener("click", copyText);
 
 });
 
@@ -18,4 +19,12 @@ function lowerCase (){
 
     document.getElementById("input-text").value = text.toLowerCase();
     
+}
+
+function copyText(){
+    const copyText = document.getElementById("input-text");  
+
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
 }
