85c976a8121883459ee63bd514a178adb67b7fa0
Thiago laian <thiago.laian@gmail.com>
2020-08-21 22:12:46 -0300

    adicionada a função de lowerCase

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", ()=>{
 
     document.getElementById("btn-uppercase").addEventListener("click", upperCase);
+    document.getElementById("btn-lowercase").addEventListener("click", lowerCase);
 
 });
 
@@ -9,5 +10,12 @@ function upperCase (){
     const text = document.getElementById("input-text").value;
 
     document.getElementById("input-text").value = text.toUpperCase();
+    
+}
+
+function lowerCase (){
+    const text = document.getElementById("input-text").value;
+
+    document.getElementById("input-text").value = text.toLowerCase();
     
 }
